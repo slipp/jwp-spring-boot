f78065a3366effabe3dab6052820fbfd708ef4dd
javajigi <javajigi@gmail.com>
2017-05-11 14:25:12 +0900

    UserController가 UserService에 직접 접근하도록 수정함.

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -2,9 +2,10 @@ package net.slipp.web;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,48 +15,45 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.slipp.domain.User;
-import net.slipp.domain.UserRepository;
 import net.slipp.security.LoginUser;
+import net.slipp.service.UserService;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 	private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
-	@Autowired private UserRepository userRepository;
-	
-	@GetMapping("/form")
-	public String form() {
-		return "/user/form";
-	}
-	
-	@PostMapping("")
-	public String create(User user) {
-		log.debug("create user : {}", user);
-		userRepository.save(user);
-		return "redirect:/users";
-	}
-	
-	@GetMapping("")
-	public String list(Model model) {
-		List<User> users = userRepository.findAll();
-		log.debug("user size : {}", users.size());
-		model.addAttribute("users", users);
-		return "/user/list";
-	}
-	
-	@GetMapping("/{id}/form")
-	public String updateForm(@LoginUser User loginUser, @PathVariable long id, Model model) {
-		model.addAttribute("user", userRepository.findOne(id));
-		return "/user/updateForm";
-	}
-	
-	@PutMapping("/{id}")
-	public String update(@LoginUser User loginUser, @PathVariable long id, User target) {
-		User original = userRepository.findOne(id);
-		original.update(loginUser, target);
-		userRepository.save(original);
-		return "redirect:/users";
-	}
-	
+    @Resource(name = "userService")
+    private UserService userService;
+    
+    @GetMapping("/form")
+    public String form() {
+        return "/user/form";
+    }
+    
+    @PostMapping("")
+    public String create(User user) {
+        userService.add(user);
+        return "redirect:/users";
+    }
+    
+    @GetMapping("")
+    public String list(Model model) {
+        List<User> users = userService.findAll();
+        log.debug("user size : {}", users.size());
+        model.addAttribute("users", users);
+        return "/user/list";
+    }
+    
+    @GetMapping("/{id}/form")
+    public String updateForm(@LoginUser User loginUser, @PathVariable long id, Model model) {
+        model.addAttribute("user", userService.findById(id));
+        return "/user/updateForm";
+    }
+    
+    @PutMapping("/{id}")
+    public String update(@LoginUser User loginUser, @PathVariable long id, User target) {
+        userService.update(loginUser, id, target);
+        return "redirect:/users";
+    }
 }
