d4cbd39df00bb248124ce7ae2946adc922d777df
javajigi <javajigi@gmail.com>
2017-05-20 07:02:46 +0900

    refactor cherry pick

--- cherry_pick.sh
+++ cherry_pick.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-REPOSITORY_DIR=/cygdrive/d/jwp-workspace/workspace/cherry-jwp-spring-boot
+REPOSITORY_DIR=~/web-workspace/jwp-woowahan/cherry-jwp-spring-boot
 
 # BRANCHES=("step1-user" "step2-refactoring" "step3-session" "step4-loginuser-logging" "step5-orm" "step6-refactoring-entity" "step7-restful-api" "step8-rest-assured" "step9-oop-delete-question" "step10-delete-question-completed" "step11-spring-profile")
-BRANCHES=("step5-orm" "step6-refactoring-entity" "step7-restful-api" "step8-rest-assured" "step9-oop-delete-question" "step10-delete-question-completed")
-COMMIT_REVISION=210bb07
+BRANCHES=("step5-orm" "step6-refactoring-entity" "step7-restful-api" "step8-rest-assured" "step9-oop-delete-question")
+COMMIT_REVISION=7ce43c1
 
 rm -rf $REPOSITORY_DIR
 
