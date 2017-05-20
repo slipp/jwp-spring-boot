f3d4604ac9a64ef98f78e3568058e95355c95410
javajigi <javajigi@gmail.com>
2017-05-20 11:36:01 +0900

    test login

---
+++ src/test/java/net/slipp/service/UserServiceTest.java
@@ -0,0 +1,49 @@
+package net.slipp.service;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import net.slipp.domain.User;
+import net.slipp.domain.UserRepository;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+    @Mock
+    private UserRepository userRepository;
+    
+    @InjectMocks
+    private UserService userService;
+    
+    @Test
+    public void login_success() {
+        User user = new User("sanjigi", "password", "name", "javajigi@slipp.net");
+        when(userRepository.findByUserId(user.getUserId())).thenReturn(Optional.of(user));
+        
+        User loginUser = userService.login(user.getUserId(), user.getPassword());
+        assertThat(loginUser, is(user));
+    }
+    
+    @Test(expected = IllegalStateException.class)
+    public void login_failed_when_user_not_found() {
+        when(userRepository.findByUserId("sanjigi")).thenReturn(Optional.empty());
+        
+        userService.login("sanjigi", "password");
+    }
+    
+    @Test(expected = IllegalStateException.class)
+    public void login_failed_when_mismatch_password() {
+        User user = new User("sanjigi", "password", "name", "javajigi@slipp.net");
+        when(userRepository.findByUserId(user.getUserId())).thenReturn(Optional.of(user));
+        
+        userService.login(user.getUserId(), user.getPassword() + "2");
+    }
+}
