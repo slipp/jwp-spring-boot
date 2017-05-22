fd53c6d8dafb78426dc2c1a079c590606e440d11
javajigi <javajigi@gmail.com>
2017-05-22 09:34:18 +0900

    rename IntegrationTest to AcceptanceTest

--- src/test/java/support/test/AcceptanceTest.java
+++ src/test/java/support/test/AcceptanceTest.java
@@ -9,6 +9,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public abstract class IntegrationTest {
+public abstract class AcceptanceTest {
 	@Autowired protected TestRestTemplate template;
 }
