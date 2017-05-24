aeb5a2150d1840edaf1ce904934958f637778b22
javajigi <javajigi@gmail.com>
2017-05-24 18:51:07 +0900

    삭제 답변은 조회할 수 없도록 설정 추가함.

--- src/main/java/net/slipp/domain/Answers.java
+++ src/main/java/net/slipp/domain/Answers.java
@@ -8,11 +8,14 @@ import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
 import javax.persistence.OrderBy;
 
+import org.hibernate.annotations.Where;
+
 import net.slipp.CannotDeleteException;
 
 @Embeddable
 public class Answers {
     @OneToMany(mappedBy="question", cascade = CascadeType.ALL)
+    @Where(clause = "deleted = false")
     @OrderBy("id ASC")
     private List<Answer> answers = new ArrayList<>();
 
